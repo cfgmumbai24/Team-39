816398719795b8c57aecccbdf35e6c83fdb592ec
adnan ali <adnanali11875@gmail.com>
2024-06-16 02:03:44 +0530

    added nav-more

--- ui/src/pages/Foundation.js
+++ ui/src/pages/Foundation.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
+import Updateoredit from './updateoredit';
 
 const FoundationForm = () => {
   const [formValues, setFormValues] = useState({
