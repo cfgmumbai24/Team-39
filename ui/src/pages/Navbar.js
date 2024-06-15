ec6862918d8136bd3fc6ee88be5fa389d744c1f8
Mayank <mayank290404gupta@gmail.com>
2024-06-15 19:44:16 +0530

    navbar

--- ui/src/pages/Navbar.js
+++ ui/src/pages/Navbar.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
 
-function Navbar() {
-  return (
-    <div className='bg-[grey] h-[10%]'>
-      hello
-    </div>
-  )
-}
-
-export default Navbar
+const Navbar = () => {
+    return (
+      <nav className="bg-white border-b-2 border-gray-200 px-4 py-2.5 sm:px-6 lg:px-8">
+        <div className="container flex flex-wrap items-center justify-start gap-x-[10%]">
+          <div className="text-xl font-bold text-gray-900 hover:bg-purple-300 px-3 py-2 rounded-lg">Home</div>
+          <div className="text-xl font-bold text-gray-900 hover:bg-purple-300 px-3 py-2 rounded-lg">Resources</div>
+          <div className="text-xl font-bold text-gray-900 hover:bg-purple-300 px-3 py-2 rounded-lg">About</div>
+          <div className="text-xl font-bold text-gray-900 hover:bg-purple-300 px-3 py-2 rounded-lg">Contact Us</div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
