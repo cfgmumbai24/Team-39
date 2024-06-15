09ea271b285f3901f609bbe2f47c58dae6a82889
adnan ali <adnanali11875@gmail.com>
2024-06-16 03:35:39 +0530

    added NumericalsForm

---
+++ ui/src/pages/NumericalForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Updateoredit from './updateoredit'
+const NumericalForm = () => {
+  return (
+    <div>
+        <Updateoredit/>
+    <div>
+        <form className="w-full max-w-lg mx-auto mt-10 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <div className="mb-4">
+    <label className="block text-gray-700 text-sm font-bold mb-2" >
+      Maths
+    </label>
+    <input
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      type="text"
+      placeholder="Enter Maths Marks"
+    />
+  </div>
+
+
+  
+
+
+  <div className="flex items-center justify-between">
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="button"
+      onClick={() => {
+        
+      }}
+    >
+      Submit
+    </button>
+  </div>
+</form>
+    </div>
+    </div>
+  )
+}
+
+export default NumericalForm
