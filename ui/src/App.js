d8d039694c62a843cf35ab8ae340b50cf51ecb04
adityasingh0804 <singhaditya200408apr@gmail.com>
2024-06-15 19:17:40 +0530

    router setup

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Progress from './components/Progress';
+import Edit from './components/Edit';
 const App = () => {
   return (
     <div className=' text-gray-500 bg-green-700'>
-      Hello
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/progress' element={<Progress />} />
+          <Route path='/edit' element={<Edit />} />
+        </Routes>
+      </Router>
+
+
+      
     </div>
   )
 }
