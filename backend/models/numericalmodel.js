e6307ddc98157ed92396e37f3215ad4b1d30ce7c
Rohit <rohit3035132@gmail.com>
2024-06-15 19:15:33 +0530

    added numericalmodel

---
+++ backend/models/numericalmodel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const numericalModelSchema = new mongoose.Schema({
+    sid : {
+        type: Number,
+        required: true
+    },
+    maths : {
+        type: Number,
+        required: true
+    },
+    avg : {
+        type: Number,
+        default: 0
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const NumericalModel = mongoose.model('NumericalModel', numericalModelSchema);
+
+export default NumericalModel;
