e3cb1ba695047a518485bd05fde2fc6e7bafc50f
mahimak13 <130887188+mahimak13@users.noreply.github.com>
2024-06-16 10:21:58 +0530

    app:

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/firebase_options.dart';
-import 'package:flutter_app/home_screen.dart';
 import 'package:flutter_app/sign_in.dart';
+import 'package:flutter_app/home_screen.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: DetailsPage(),
     );
 
   }
